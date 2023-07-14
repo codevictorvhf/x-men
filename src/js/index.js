71112b235fdacef6c85f4f5f429688580b3ee168
“Victor <victorvf11@outlook.com>
2023-07-14 16:05:21 -0300

    commit x-men js

--- src/js/index.js
+++ src/js/index.js
@@ -2,22 +2,40 @@ const characters = document.querySelectorAll(".character");
 
 characters.forEach((character) => {
   character.addEventListener("mouseenter", () => {
-    const characterSelected = document.querySelector(".selected");
-    characterSelected.classList.remove("selected");
+    removeCharacterSelection();
 
     character.classList.add("selected");
 
-    const bigCharacterImage = document.querySelector(".big-character");
+    changeSelectedCharacterImage(character);
 
-    const idCharacter = character.attributes.id.value;
-    bigCharacterImage.src = `./src/images/card-${idCharacter}.png`;
+    changeSelectedCharacterName(character);
 
-    const characterName = document.getElementById("character-name");
-    characterName.innerText = character.getAttribute("data-name");
-
-    const characterDescription = document.getElementById(
-      "character-description"
-    );
-    characterDescription.innerText = character.getAttribute("data-description");
+    changeCharacterDescription(character);
   });
+
+  if (window.innerWidth < 450) {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
 });
+
+function changeCharacterDescription(character) {
+  const characterDescription = document.getElementById("character-description");
+  characterDescription.innerText = character.getAttribute("data-description");
+}
+
+function changeSelectedCharacterName(character) {
+  const characterName = document.getElementById("character-name");
+  characterName.innerText = character.getAttribute("data-name");
+}
+
+function changeSelectedCharacterImage(character) {
+  const bigCharacterImage = document.querySelector(".big-character");
+
+  const idCharacter = character.attributes.id.value;
+  bigCharacterImage.src = `./src/images/card-${idCharacter}.png`;
+}
+
+function removeCharacterSelection() {
+  const characterSelected = document.querySelector(".selected");
+  characterSelected.classList.remove("selected");
+}
